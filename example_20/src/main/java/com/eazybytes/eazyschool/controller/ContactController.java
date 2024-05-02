63069b600a4e249d04ec3154534590c73e0abad0
Satish Maharjan <Satish.maharjan77@gmail.com>
2024-05-02 16:08:24 +0545

    Server side validation using valdation annotations

--- example_20/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ example_20/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -2,11 +2,14 @@ package com.eazybytes.eazyschool.controller;
 
 import com.eazybytes.eazyschool.model.ContactDto;
 import com.eazybytes.eazyschool.service.ContactService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,7 +21,8 @@ public class ContactController {
     private ContactService contactService;
 
     @RequestMapping("/contact")
-    public String displayContact(){
+    public String displayContact(Model model)
+    {   model.addAttribute("contact",new ContactDto());
         return "contact.html";
     }
 
@@ -38,9 +42,13 @@ public class ContactController {
      */
 
     @RequestMapping(value = "/saveMsg",method = RequestMethod.POST)
-    public ModelAndView saveMessage( ContactDto contact) {
+    public String saveMessage(@Valid @ModelAttribute("contact") ContactDto contact, Errors errors) {
+        if (errors.hasErrors()){
+            log.error("contact validation failed due to" + errors.toString() );
+            return "contact.html";
+        }
          contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 
 }
